cd94a9d559b75962260819d45971dbff4451784b
Nataliya <https.ka@gmail.com>
2017-08-24 20:02:12 +0000

    fibonachi first

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let n = +prompt('enter n');
+
+function fibonacchi(n){
+  if ( n == 0 ) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fibonacchi(n-2) + fibonacchi(n-1);
+  }
+}
+document.write(fibonacchi(n));
