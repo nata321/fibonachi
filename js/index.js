c9084e1860470e8c77f4158c75eaf7f489642ee1
Nataliya <https.ka@gmail.com>
2017-08-24 21:52:44 +0000

    fib2

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
+// let n = +prompt('enter n');
+
+// function fibonacchi(n){
+//   if ( n == 0 ) {
+//     return 0;
+//   } else if (n == 1) {
+//     return 1;
+//   } else {
+//     return fibonacchi(n-2) + fibonacchi(n-1);
+//   }
+// }
+// document.write(fibonacchi(n));
+
+
 let n = +prompt('enter n');
 
-function fibonacchi(n){
-  if ( n == 0 ) {
-    return 0;
-  } else if (n == 1) {
-    return 1;
-  } else {
-    return fibonacchi(n-2) + fibonacchi(n-1);
+function fib(n) {
+  var f, t1, t2;
+
+  for (i = 1; i <= n; i++) {
+    f = t1 + t2 || 1;
+    t1 = t2;
+    t2 = f;
   }
+   return f;
 }
-document.write(fibonacchi(n));
+ document.write(fib(n));
