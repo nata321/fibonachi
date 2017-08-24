33f5d316bd9e35ff389276d299b6d51a94c23b71
Nataliya <https.ka@gmail.com>
2017-08-24 22:02:20 +0000

    fib3

--- js/index.js
+++ js/index.js
@@ -1,27 +1,25 @@
-// let n = +prompt('enter n');
-
-// function fibonacchi(n){
-//   if ( n == 0 ) {
-//     return 0;
-//   } else if (n == 1) {
-//     return 1;
-//   } else {
-//     return fibonacchi(n-2) + fibonacchi(n-1);
-//   }
-// }
-// document.write(fibonacchi(n));
+let n = +prompt('enter n');
+function fibonacchi(n){
+  if ( n == 0 ) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fibonacchi(n-2) + fibonacchi(n-1);
+  }
+}
+document.write(fibonacchi(n));
 
 
 let n = +prompt('enter n');
-
-function fib(n) {
-  var f, t1, t2;
-
-  for (i = 1; i <= n; i++) {
-    f = t1 + t2 || 1;
-    t1 = t2;
-    t2 = f;
+ function fib(n) {
+  var a = 1,
+    b = 1;
+  for (var i = 3; i <= n; i++) {
+    var c = a + b;
+    a = b;
+    b = c;
   }
-   return f;
+  return b;
 }
- document.write(fib(n));
+document.write(fib(n));
